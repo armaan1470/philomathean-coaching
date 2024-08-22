ae345a01e33c13438b9908f88b0fc3b73a62e593
GulamAhmad <ahmedgulam83@gmail.com>
2024-08-22 22:07:47 +0530

    banner image changes and some minor ui changes

--- src/components/BannerSlider/BannerSlider.jsx
+++ src/components/BannerSlider/BannerSlider.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import banner1 from "../../assets/images/slider-1.jpg";
-import './BannerSlider.css'; // Import a CSS file for custom styles
+import banner2 from "../../assets/images/main_banner_1.jpg";
+import banner3 from "../../assets/images/main_banner_2.jpg";
+import "./BannerSlider.css"; // Import a CSS file for custom styles
 import Counter from "../../components/Counter/Counter";
 
 const BannerSlider = () => {
@@ -19,20 +21,37 @@ const BannerSlider = () => {
 
   return (
     <>
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-          <img src={banner1} alt="Image 1" className="slider-image" loading='lazy'/>
-        </div>
-        <div>
-          <img src={banner1} alt="Image 2" className="slider-image" loading='lazy'/>
-        </div>
-        {/* Add more slides as needed */}
-      </Slider>
+      <div className="slider-container">
+        <Slider {...settings}>
+          <div>
+            <img
+              src={banner2}
+              alt="Image 1"
+              className="slider-image"
+              loading="lazy"
+            />
+          </div>
+          <div>
+            <img
+              src={banner3}
+              alt="Image 2"
+              className="slider-image"
+              loading="lazy"
+            />
+          </div>
+          <div>
+            <img
+              src={banner1}
+              alt="Image 2"
+              className="slider-image"
+              loading="lazy"
+            />
+          </div>
+          {/* Add more slides as needed */}
+        </Slider>
       </div>
       <Counter />
     </>
-    
   );
 };
 
